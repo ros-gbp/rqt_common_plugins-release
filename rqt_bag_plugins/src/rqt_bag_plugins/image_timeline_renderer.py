6778739767d21d08f1c2dbd1fec7abee88008498
Dirk Thomas <dthomas@osrfoundation.org>
2016-11-02 11:04:39 -0700

    Rebase from 'upstream'

--- rqt_bag_plugins/src/rqt_bag_plugins/image_timeline_renderer.py
+++ rqt_bag_plugins/src/rqt_bag_plugins/image_timeline_renderer.py
@@ -34,7 +34,11 @@ import rospy
 
 # HACK workaround for upstream pillow issue python-pillow/Pillow#400
 import sys
-if 'PyQt5' in sys.modules:
+from python_qt_binding import QT_BINDING_MODULES
+if (
+    not QT_BINDING_MODULES['QtCore'].__name__.startswith('PyQt5') and
+    'PyQt5' in sys.modules
+):
     sys.modules['PyQt5'] = None
 from PIL import Image
 from PIL.ImageQt import ImageQt
@@ -123,7 +127,7 @@ class ImageTimelineRenderer(TimelineRenderer):
             if width == 1:
                 break
 
-        painter.setPen(QPen(QBrush(Qt.black)))
+        painter.setPen(QPen(Qt.black))
         painter.setBrush(QBrush(Qt.transparent))
         if width == 1:
             painter.drawRect(x, y, thumbnail_x - x, height - thumbnail_gap - 1)
